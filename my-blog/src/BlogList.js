68a050a0c5aa5e5bef0d4208f5dffaaaa03ba814
Christine Lo <christine.lo.hayes@gmail.com>
2022-02-11 18:28:24 -0500

    adduseeffect

--- my-blog/src/BlogList.js
+++ my-blog/src/BlogList.js
@@ -5,8 +5,10 @@ const BlogList = ({blogs,handleDelete}) => {
 {
         blogs.map((blog)=>(
           <div className='blog-preview' key={blog.id}>
-            <h1>Written by {blog.title}</h1>
-            <p>blog.author</p>
+            <h1>{blog.title}</h1>
+            <h3>{blog.body}</h3>
+            <p>Written by {blog.author}</p>
+
             <button onClick={()=>handleDelete(blog.id)}> delete blog </button>
             </div>
       ))
