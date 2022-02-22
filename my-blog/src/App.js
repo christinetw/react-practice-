69374acb85dc3fd5c4e9f206f12cf3e58b3997e3
Christine Lo <christine.lo.hayes@gmail.com>
2022-02-21 22:36:36 -0500

    addrouter

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,17 +1,27 @@
 
 import NavBar from './NavBar';
 import Home from './Home';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Create from './Create';
 
 function App() {
 
   return (
-    <div className="App">
-      <NavBar/>
-      <div className="content">
-        <Home/>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Router exact path='/'>
+              <Home />
+            </Router>
+            <Router path='/create'>
+              <Create />
+            </Router>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
